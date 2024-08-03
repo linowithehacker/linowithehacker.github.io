fab0b652611f121440159dbe0dee8e63e81269b3
linowithehacker <145560589+linowithehacker@users.noreply.github.com>
2024-08-03 17:36:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Popup Example</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="popup" id="popup">
+        <p>There are no cookies and no JavaScript, just simple HTML, CSS, and PHP.</p>
+        <button class="close-button" onclick="closePopup()">Close</button>
+    </div>
+    <script src="script.js"></script>
+<h1>This is my website, and you will not find telemetry here.</h1>
+</body>
+</html>
